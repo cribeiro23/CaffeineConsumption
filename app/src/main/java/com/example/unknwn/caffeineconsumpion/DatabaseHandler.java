0a0e7d3afdc159ec4474651e90e9bfaf470ef3c4
Carlos Eduardo Matos Ribeiro <cmatosribeiro@gmail.com>
2017-10-22 00:00:37 -0700

    Included pull functionality to database handler

--- app/src/main/java/com/example/unknwn/caffeineconsumpion/DatabaseHandler.java
+++ app/src/main/java/com/example/unknwn/caffeineconsumpion/DatabaseHandler.java
@@ -65,5 +65,22 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 	    db.close(); // Closing database connection
 	}
 	
+	// Pulling data from contact
+	  // Getting single contact
+    Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+ 
+        Cursor cursor = db.query(TABLE_CONTACTS, new String[] { KEY_WEIGHT,
+                KEY_CAFFEINEMETABOLITE, KEY_CAFFEINETOLERANCE }, KEY_ID + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+ 
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+        // return contact
+        return contact;
+    }
+	
 	
 }
